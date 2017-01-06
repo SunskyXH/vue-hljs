50b5b6d66c1d5fd7ecadba9cf517300f765a4980
SunskyXH <y445360776@qq.com>
2017-01-06 10:27:27 +0800

    first edition

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import Hljs from 'highlight.js'
+
+var vueHljs = {}
+
+vueHljs.install = function (Vue) {
+  Vue.directive('highlight', {
+    deep: true,
+    bind: function (el) {
+      let blocks = el.querySelectorAll('pre code')
+      Array.prototype.forEach.call(blocks, Hljs.highlightBlock)
+    }
+  })
+}
+
+//CommonJS
+if (typeof exports == 'object') {
+  module.exports = vueHljs
+}
+//AMD
+else if (typeof define == 'function' && define.amd) {
+  define([], function () { return vueHljs })
+}
+//using Vue.use()
+else if (window.Vue) {
+  window.VueHljs = vueHljs
+  Vue.use(vueHljs)
+}
