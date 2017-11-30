c47073db5bced1b918e5c1f3ee653bf958dacabd
SunskyXH <y445360776@qq.com>
2017-11-30 17:43:28 +0800

    delete AMD

--- src/index.js
+++ src/index.js
@@ -14,12 +14,6 @@ vueHljs.install = (Vue) => {
 if (typeof exports == "object") {
   module.exports = vueHljs;
 } 
-//AMD
-else if (typeof define == "function" && define.amd) {
-  define([], function() {
-    return vueHljs;
-  });
-}
 //using Vue.use()
 else if (window.Vue) {
   window.VueHljs = vueHljs;
